162a35d1086b8a40edb270284696d27abcf8271b
Espen H. Kristensen <espen.kristensen@kantega.no>
2022-12-13 13:55:23 +0100

    02 part 2 done

--- 02/main.py
+++ 02/main.py
@@ -1,4 +1,4 @@
-def determine_game(str_a, str_b):
+def determine_game_one(str_a, str_b):
     choice_a = None
     choice_b = None
     result = None
@@ -49,6 +49,42 @@ def determine_game(str_a, str_b):
 
     return result, choice_a, choice_b
 
+def determine_game_two(your_choice, desired):
+    choice_a = None
+    choice_b = None
+    result = None
+
+    # determine your choice
+    if your_choice == "A":
+        choice_a = 'rock'
+        if desired == "X": # lose
+            choice_b = 'scissors'
+        elif desired == "Y": # draw
+            choice_b = 'rock'
+        else: # win
+            choice_b = 'paper'
+    elif your_choice == "B":
+        choice_a = 'paper'
+        if desired == "X": # lose
+            choice_b = 'rock'
+        elif desired == "Y": # draw
+            choice_b = 'paper'
+        else: # win
+            choice_b = 'scissors'
+    elif your_choice == "C":
+        choice_a = 'scissors'
+        if desired == "X": # lose
+            choice_b = 'paper'
+        elif desired == "Y": # draw
+            choice_b = 'scissors'
+        else: # win
+            choice_b = 'rock'
+
+    your_choice = choice_a
+    my_choice = choice_b
+
+    return your_choice, my_choice
+
 def calculate_score(my_choice, result):
     scores_result = {'B': 6, 'draw': 3, 'A': 0}
     scores_choice = {'rock': 1, 'paper': 2, 'scissors': 3}
@@ -60,20 +96,37 @@ def calculate_score(my_choice, result):
     return final_score
 
 
+def translate_res(desired_res):
+    if desired_res == 'X':
+        return 'A'
+    elif desired_res == 'Y':
+        return 'draw'
+    return 'B'
+
+
 if __name__ == "__main__":
     draw_score = 0
     total_score_one = 0
+    total_score_two = 0
 
     with open("input.txt") as infile:
-        for line in infile:
+        for i, line in enumerate(infile):
             # == PART ONE SECTION == 
             your_choice, my_choice = line.strip().split(" ")
 
-            res, your_choice, my_choice = determine_game(your_choice, my_choice)
+            res, your_choice, my_choice = determine_game_one(your_choice, my_choice)
             score = calculate_score(my_choice, res)
             total_score_one += score
 
             # == PART TWO SECTION ==
             your_choice_two, desired_res = line.strip().split(" ")
+            your_choice_two, my_choice_two = determine_game_two(your_choice_two, desired_res)
+            desired_res = translate_res(desired_res)
+            score_two = calculate_score(my_choice_two, desired_res)
+            #print(your_choice_two, score_two)
+            total_score_two += score_two
+            #if i > 50:
+            #    break
 
     print("Final score, part 1 method: ", total_score_one)
+    print("Final score, part 2 method: ", total_score_two)
