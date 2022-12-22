f11a54ec0ff04e90eb65d6c1e56307621d6f3b69
Espen H. Kristensen <espenhkristensen@gmail.com>
2022-12-22 20:47:30 +0100

    04 part 2 working

--- 04/main.py
+++ 04/main.py
@@ -3,7 +3,9 @@ if __name__ == '__main__':
 
     with open("input.txt", "r") as infile:
         total_one = 0
+        total_two = 0
         for line in infile:
+            success_two = False
             pair_a, pair_b = line.split(",")
             pair_a_min, pair_a_max = pair_a.split("-")
             pair_b_min, pair_b_max = pair_b.split("-")
@@ -15,12 +17,28 @@ if __name__ == '__main__':
             #print(pair_a, pair_b)
             #print(pair_a_min, pair_a_max, pair_b_min, pair_b_max)
 
-            # check B contained in A
+            # check B entirely contained in A
             if (pair_b_min >= pair_a_min) and (pair_b_max <= pair_a_max):
                 total_one += 1
-                print(line, total_one)
+                #print(line, total_one)
+            # check A entirely contained in B
             elif (pair_a_min >= pair_b_min) and (pair_a_max <= pair_b_max):
                 total_one += 1
-                print(line, total_one)
+                #print(line, total_one)
+
+            # check B partially contained in A
+            if (pair_b_min >= pair_a_min) and (pair_b_min <= pair_a_max):
+                success_two = True
+            if (pair_b_max >= pair_a_min) and (pair_b_max <= pair_a_max):
+                success_two = True
+            # check A partially contained in B
+            if (pair_a_min >= pair_b_min) and (pair_a_min <= pair_b_max):
+                success_two = True
+            if (pair_a_max >= pair_b_min) and (pair_a_max <= pair_b_max):
+                success_two = True
+
+            if success_two:
+                total_two += 1
 
         print("Answer to part one: ", total_one)
+        print("Answer to part two: ", total_two)
